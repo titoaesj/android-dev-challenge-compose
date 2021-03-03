9b9e71fe81c944ff890d7234e66bb75ca63e1242
titoaesj <titoaesj@mbp-de-titoaesj.home>
2021-03-03 02:02:44 -0300

    check app:spotlessApply

--- app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.app.Activity
@@ -7,9 +22,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,7 +76,6 @@ fun headDetailComp(activity: Activity, dog: Dog) {
                     .fillMaxSize()
             )
 
-
             Box(modifier = Modifier.padding(start = 16.dp, top = 16.dp)) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_black),
@@ -67,7 +90,6 @@ fun headDetailComp(activity: Activity, dog: Dog) {
 
                 )
             }
-
         }
 
         infoDogComp(dog)
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,24 +16,29 @@
 package com.example.androiddevchallenge
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Space
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,14 +48,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
-import androidx.lifecycle.LifecycleOwner
 import com.example.androiddevchallenge.model.Dog
-import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
@@ -76,7 +76,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
 
 @Composable
 fun LogoComp() {
@@ -108,7 +107,7 @@ fun HeadComp() {
     }
 }
 
-//@Preview("CardItemComp", widthDp = 379, heightDp = 175)
+// @Preview("CardItemComp", widthDp = 379, heightDp = 175)
 
 @Composable
 fun infoDogComp(dog: Dog?) {
@@ -160,7 +159,6 @@ fun infoDogComp(dog: Dog?) {
             )
             Text(text = dog?.heightFemale.toString(), style = typography.body1)
         }
-
     }
 }
 
@@ -178,7 +176,6 @@ fun CardItemComp(dog: Dog?, activity: Activity) {
                 bundle.putParcelable("dog_param", dog)
                 intent.putExtras(bundle)
                 activity.startActivity(intent)
-
             },
         elevation = 4.dp
     ) {
@@ -195,27 +192,22 @@ fun CardItemComp(dog: Dog?, activity: Activity) {
             )
 
             infoDogComp(dog = dog)
-
         }
-
-
     }
-
 }
 
-
-//@Preview("Light Theme", widthDp = 411, heightDp = 731)
-//@Composable
-//fun LightPreview() {
+// @Preview("Light Theme", widthDp = 411, heightDp = 731)
+// @Composable
+// fun LightPreview() {
 //    MyTheme {
 //        MyApp(viewModel = null, null)
 //    }
-//}
+// }
 
-//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-//@Composable
-//fun DarkPreview() {
+// @Preview("Dark Theme", widthDp = 360, heightDp = 640)
+// @Composable
+// fun DarkPreview() {
 //    MyTheme(darkTheme = true) {
 //        MyApp()
 //    }
-//}
+// }
